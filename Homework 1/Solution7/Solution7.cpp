4a5ed0e18f998bce1acf2ed80180d589aac66c7e
SpinSpinSugar <88590354+SpinSpinSugar@users.noreply.github.com>
2021-09-21 16:50:46 +0300

    Update Solution7.cpp

--- Homework 1/Solution7/Solution7.cpp
+++ Homework 1/Solution7/Solution7.cpp
@@ -7,9 +7,9 @@ int main()
     setlocale(LC_ALL, "Russian");
     int a, b, c;
     cin >> a >> b;
-    a = a + b;
-    b = a - b;
-    a = a - b;
+    c = a;
+    a = b;
+    b = c;
     cout << a << " " << b;
     return EXIT_SUCCESS;
 }
