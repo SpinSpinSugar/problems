dca985fa24fb9ae972449657ce9a39614bff67ff
st095228 <st095228@student.spbu.ru>
2021-09-21 22:41:07 +0300

    fix 3.0

---
+++ Homework 1/Solution6/Solution6.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <locale.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int N;
+    cin >> N;
+    cout << (N / 2) * 2 + 2 * ((((2 * N + 1) % 2) + 1) / 2 + ((N + 1) % 2) * (((2 * N + 1) % 2) - 1) / 2); // костылим без if :-)
+    return EXIT_SUCCESS;
+}
