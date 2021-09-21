62afe18d874cb2fd98a8f0fcc0fbd68fec159b0c
st095228 <st095228@student.spbu.ru>
2021-09-21 23:30:15 +0300

    Homework 1

---
+++ Solution/Solution10/Solution10.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <locale.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a, b;
+    cin >> a >> b;
+    cout << (a + b + (a - b) * ((2 * (a-b) + 1) % 2)) / 2;
+    return EXIT_SUCCESS;
+}
---
+++ Solution/Solution2/Solution2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <locale.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int N, K;
+    cin >> N >> K;
+    cout << N / K;
+    return EXIT_SUCCESS;
+}
