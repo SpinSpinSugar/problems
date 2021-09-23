7dc80b37581c2c89182b5727bab7dc81c79d926c
Зернов Семен Николаевич <st095228@student.spbu.ru>
2021-09-23 12:22:05 +0300

    dodelat'

---
+++ 2021.09.23-Homework-2/Task2/Task2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<locale.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "Russian");
+	int N;
+	cin >> N;
+	if ((N % 4) && (N % 100) || (N % 400))
+	{
+		cout << "YES";
+	}
+	else
+	{
+		cout << "NO";
+	}
+    return EXIT_SUCCESS;
+}
---
+++ 2021.09.23-Homework-2/Task3/Task3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<locale.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "Russian");
+	int k;
+	int m;
+	int n;
+	cin >> k >> m >> n;
+}
