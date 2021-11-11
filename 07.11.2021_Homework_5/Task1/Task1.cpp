a34c15a100bf1b3030f4f0993938fd14bf5da99c
st095228 <st095228@student.spbu.ru>
2021-11-11 23:06:19 +0300

    fix

---
+++ 07.11.2021_Homework_5/Task1/Task1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    int a = 0;
+    int n = 0;
+    int m = 0;
+    cin >> a >> n >> m;
+    int nbit = (a & (1 << n)) >> n; //вычисляем n-ый бит числа а
+                                    //                       a = xxzxxyxx
+                                    // (n = 2), (тогда 1 << n) = 00000100
+                                    //            a & (1 << n) = 00000y00
+                                    //     (a & (1 << n)) >> n = y
+    a = a & (~(1 << n)); //обнуляем n-ый бит: a = xxzxx0xx
+    int mbit = (a & (1 << m)) >> m; //m-ый бит
+                                    //                       a = xxzxx0xx
+                                    // (m = 2), (тогда 1 << m) = 00100000
+                                    //            a & (1 << m) = 00z00000
+                                    //     (a & (1 << m)) >> m = z
+    a = a & (~(1 << m)); //обнуляем m-ый бит: a = xx0xx0xx
+    a = a | (nbit << m) | (mbit << n); //              a = xx0xx0xx
+                                       //  | (nbit << m) = 00y00000
+                                       //  | (mbit << n) = 00000z00
+                                       //                = xxyxxzxx
+    cout << a;
+    return EXIT_SUCCESS;
+}
---
+++ 07.11.2021_Homework_5/Task3/Task3.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    int a = 0;
+    cin >> a;
+    if (a >= 0)
+    {
+        cout << a;
+    }
+    else
+    {
+        a = ~a;
+        int c = 0;
+        int final_sum = 0;
+        int sum = 0;
+        for (int i = 7; i >= 0; i--)
+        {
+            sum = ((a >> i) ^ (1 >> i)) ^ c; // Full Adder
+            c = ((a >> i) & (1 >> i)) | ((a >> i) & c) | ((1 >> i) & c);
+            final_sum = (final_sum | sum) << 1;
+        }
+        final_sum = final_sum >> 1;
+        if ((final_sum & 1) != 1)
+        {
+            cout << (final_sum | 2);
+        }
+        else
+        {
+            cout << (final_sum);
+        }
+
+    }
+    return EXIT_SUCCESS;
+}
