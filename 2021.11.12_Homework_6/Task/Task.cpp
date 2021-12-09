04223f08a53f559c8b165a917bcb7b96d4b5b1f1
st095228 <st095228@student.spbu.ru>
2021-12-09 03:06:03 +0300

    no vector fix

--- 2021.11.12_Homework_6/Task/Task.cpp
+++ 2021.11.12_Homework_6/Task/Task.cpp
@@ -1,93 +1,155 @@
 #include <iostream>
 #include <locale.h>
-#include <vector>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	vector<int> array(0);
-	int n = 0;
-	int t = 0;
-	int b = 0;
+	int capacity = 1;
+	int* array = new int[capacity] { 0 };
+	int choice = 0;
+	int size = 0;
+	int capacity_1 = 0;
+	int capacity_2 = 0;
+	int temp = 0;
 	string y = "";
 	while (true)
 	{
 		system("cls");
+		
+		/* DEBUG
+		cout << "Capacity: " << capacity << endl;
+		cout << "Size: " << size << endl;
+		*/
+
+		//MENU
 		setlocale(LC_ALL, "Russian");
 		cout << "Введите число" << endl;
 		cout << "0. Выход из программы." << endl;
 		cout << "1. Вывод текущего состояния массива." << endl;
-		cout << "2. Добавить элемент в конец массива. Если capacity не хватает, оно должно умножиться на 2." << endl;
+		cout << "2. Добавить элемент в конец массива. Если capacity не хватает, оно умножается на 2" << endl;
 		cout << "3. Добавить элемент в начало массива." << endl;
 		cout << "4. Удалить элемент из конца массива." << endl;
 		cout << "5. Удалить элемент из начала массива." << endl;
 		cout << "6. Развернуть массив.Элементы массива должны встать в обратном порядке." << endl;
-		cin >> n;
-		switch (n)
+		cin >> choice;
+
+		//UPDATING THE CAPACITY OF TEMPORARY ARRAYS BASED ON choice
+		capacity_1 = capacity;
+		capacity_2 = capacity;
+		if ((capacity == size) & (choice == 2))
+		{
+			capacity_1 = capacity * 2; //case 2
+		}
+		if ((capacity_2 == size) & (choice == 3))
+		{
+			capacity_2 = ++capacity; //case 3
+		}
+
+		//TEMPORARY ARRAYS INITIALIZATION WITH UPDATED CAPACITY
+		int* temp_array_1 = new int[capacity_1] { 0 };
+		int* temp_array_2 = new int[capacity_2] { 0 };
+		int* temp_array_3 = new int[capacity] { 0 };
+		int* temp_array_4 = new int[capacity] { 0 };
+
+		switch (choice)
 		{
 		case 0:
+			//MEMORY CLEANING
+			delete[] array;
+			delete[] temp_array_1;
+			delete[] temp_array_2;
+			delete[] temp_array_3;
+			delete[] temp_array_4;
 			return EXIT_SUCCESS;
 		case 1:
 			system("cls");
-			for (int i = 0; i < array.size(); i++)
+			for (int i = 0; i < size; i++)
 			{
 				cout << array[i] << " ";
 			}
-			if (array.size() == 0)
+			if (size == 0)
 			{
 				cout << "Пусто";
 			}
 			cout << endl;
-			cout << "Введите Y чтоб продолжить" << endl;
+			cout << "Введите символ, чтоб продолжить:" << endl;
 
 			cin >> y;
-			if ((y == "y") or (y == "Y"))
-			{
-				break;
-			}
 			break;
-		case 2:
+		case 2: //add an element at the end
+			system("cls");
+			for (int i = 0; i < size; ++i) //filling temporary array with old elements
+			{
+				temp_array_1[i] = array[i];
+			}
+			cout << "Введите число:" << endl;
+			cin >> temp_array_1[size]; //placing new element on last spot
+			array = temp_array_1; //updating main array
+			for (int i = 0; i < size; ++i)
+			{
+				cout << array[i] << " ";
+			}
+			capacity = capacity_1;
+			size += 1; //updating the size
+			system("cls");
+			break;
+		case 3: //add an element at the beginning
 			system("cls");
 			cout << "Введите число:" << endl;
-			cin >> t;
-			if (array.capacity() == array.size())
+			cin >> temp;
+			for (int i = 0; i < size; ++i) //filling temporary array with old elements with shift i -> i+1
 			{
-				array.reserve(2 * array.capacity());
+				temp_array_2[i + 1] = array[i];
 			}
-			array.push_back(t);
+			temp_array_2[0] = temp;
+			array = temp_array_2;
+			size += 1;
 			system("cls");
 			break;
-		case 3:
-			system("cls");
-			cout << "Введите число:" << endl;
-			cin >> b;
-			if (array.capacity() == array.size())
+		case 4: //deleting last element
+			if (size == 0)
 			{
-				array.reserve(2 * array.capacity());
+				system("cls");
+				cout << "ERROR! THERE IS NO ELEMENTS IN THIS ARRAY";
+				cout << endl;
+				cout << "Введите символ, чтоб продолжить:" << endl;
+
+				cin >> y;
 			}
-			array.insert(array.begin(), 1, b);
-			system("cls");
-			break;
-		case 4:
-			array.pop_back();
-			break;
-		case 5:
-			array.erase(array.begin());
-			break;
-		case 6:
-			system("cls");
-			vector<int> temp_array(array.size());
-			int counter = 0;
-			for (int i = array.size() - 1; i >= 0; i--)
+			else
 			{
-				temp_array[counter] = array[i];
-				counter += 1;
+				array[size - 1] = 0;
+				--size;
 			}
-			for (int i = 0; i < array.size(); i++)
+			break;
+		case 5: //deleting first element
+			if (size == 0)
 			{
-				array[i] = temp_array[i];
+				system("cls");
+				cout << "ERROR! THERE IS NO ELEMENTS IN THIS ARRAY";
+				cout << endl;
+				cout << "Введите символ, чтоб продолжить:" << endl;
+
+				cin >> y;
 			}
+			else
+			{
+				for (int i = 0; i < size; ++i) //filling temporary array with old elements with shift i + 1 -> i
+				{
+					temp_array_3[i] = array[i + 1];
+				}
+				array = temp_array_3;
+				--size;
+			}
+			break;
+		case 6: //reversing the array
+			system("cls");
+			for (int i = 0; i < size; ++i)
+			{
+				temp_array_4[size - i - 1] = array[i];
+			}
+			array = temp_array_4;
 			break;
 		}
 	}
